7ba49d9488ca4b7156bcc4b1bf8c84c8244fd46e
Lotfi Firdaouss <flotfi@astekgroup.ma>
2025-02-21 09:19:57 +0100

    creating the User entity

--- src/main/java/morocco/it/TicketSystem/entities/User.java
+++ src/main/java/morocco/it/TicketSystem/entities/User.java
@@ -1,4 +1,26 @@
 package morocco.it.TicketSystem.entities;
 
+import jakarta.persistence.*;
+import morocco.it.TicketSystem.entities.enums.Role;
+
+import java.time.Instant;
+
+@Entity
+@Table(name= "users")
+@SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt = Instant.now();
 }
